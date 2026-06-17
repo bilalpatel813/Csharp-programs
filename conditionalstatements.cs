422f33db5f95a504c31895542818a1d85b90d4c0
bilalpatel813 <Bilalpatel78600@gmail.com>
2026-06-17 23:33:28 +0530

    first commit

---
+++ conditionalstatements.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Hello World");
+		int marks = 80;
+		if(marks >= 90) 
+		{
+			Console.WriteLine("A");
+	    }
+       else if(marks >= 75)
+       {
+           Console.WriteLine("B");
+       }
+       else
+       {
+           Console.WriteLine("C");
+       }
+	} 
+	
+}
+
